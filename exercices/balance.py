a27866d3cae35d78688f915dfa6a1c5145bf4546
aMondelin <anthonydu77120@hotmail.fr>
2017-12-19 16:03:57 +0100

    Add files from previous project

---
+++ exercices/balance.py
@@ -0,0 +1,7 @@
+balance = (
+    'exercice1',
+    'exercice2',
+    'exercice3',
+    'exercice4',
+    'exercice5'
+)
---
+++ test.py
@@ -0,0 +1,17 @@
+import sys
+from numpy.random import choice
+import random
+import exercices
+
+exercices_type = ['one', 'two', 'three']
+# weights = [0.2, 0.4, 0.5]
+#
+#
+# for o in range(1,10,1):
+#     print(choice(exercices_type, p=weights))
+
+for o in range(1,10,1):
+    print(random.choice(exercices_type))
+
+exercices.bal
+print balance
