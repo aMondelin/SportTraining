e11781a3ed89c7ebbf5d8a13fa7acd3561e59c71
aMondelin <anthonydu77120@hotmail.fr>
2018-01-31 14:30:17 +0100

    Add files from previous project

--- exercices/balance.py
+++
@@ -1,7 +0,0 @@
-balance = (
-    'exercice1',
-    'exercice2',
-    'exercice3',
-    'exercice4',
-    'exercice5'
-)
--- test.py
+++ test.py
@@ -1,17 +1,43 @@
-import sys
-from numpy.random import choice
 import random
-import exercices
+import json
+import os
 
-exercices_type = ['one', 'two', 'three']
-# weights = [0.2, 0.4, 0.5]
-#
-#
-# for o in range(1,10,1):
-#     print(choice(exercices_type, p=weights))
 
-for o in range(1,10,1):
-    print(random.choice(exercices_type))
+PROJECT_ROOT = 'T:/Users/a.mondelin/python'
+PROJECT_NAME = 'SportTraining'
+EXERCICES_FOLDER = 'exercices'
+EXERCICE_TYPE_FILE = '.json'
 
-exercices.bal
-print balance
+
+EXERCICES_TYPE = ['one', 'two', 'three']
+EXERCICES_DONE = []
+exercice_count = 0
+
+
+def exercice_root(exercice_type):
+    file_name = exercice_type + EXERCICE_TYPE_FILE
+    exercice_path = os.path.join(PROJECT_ROOT, PROJECT_NAME, EXERCICES_FOLDER, file_name)
+
+    if os.path.isfile(exercice_path):
+        return exercice_path
+
+
+def get_random_exercice(exercice_type):
+    exercice_path = exercice_root(exercice_type)
+    with open(exercice_path, 'r') as read_file:
+        exercices_array = json.load(read_file)
+
+        new_exercice = random.choice(exercices_array)
+
+        while (new_exercice in EXERCICES_DONE):
+            new_exercice = random.choice(exercices_array)
+
+        EXERCICES_DONE.append(new_exercice)
+        global exercice_count += 1
+
+        return new_exercice
+
+
+for trys in range(0, 2):
+    print get_random_exercice('bras')
+
